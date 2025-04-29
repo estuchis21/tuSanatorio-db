a49f1845eb286935a273e5460c6a8a461774e805
estuchis21 <136765936+estuchis21@users.noreply.github.com>
2025-04-28 21:37:34 -0300

    commit de usuarios

---
+++ STORED PROCEDURES/insertRol.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE insertRol
+AS
+BEGIN
+    BEGIN TRY
+        INSERT INTO Roles (rol)
+        VALUES ('Paciente'), ('Médico');
+
+        PRINT 'Roles creados correctamente.';
+    END TRY
+    BEGIN CATCH
+        PRINT 'Error al agregar roles:';
+        PRINT ERROR_MESSAGE(); -- Error correcto sin variable
+    END CATCH
+END
