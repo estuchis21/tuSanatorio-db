2748288329beeb3e60e24d4cd066941eb6e876c8
estuchis21 <136765936+estuchis21@users.noreply.github.com>
2025-04-28 23:48:11 -0300

    fix: corregí todos los inserts relacionados al user

--- Ejecuciones/ejecucionesTest.sql
+++ Ejecuciones/ejecucionesTest.sql
@@ -1,4 +1,5 @@
 exec insertarUsuario @DNI = 49809765, @nombres = 'Lautaro Bustamente', @telefono = '2364308570', @mail = 'lauti_yb2005@gmail.com', @username = 'lauuuti.y', @contrasena = 'Svaaea01', @rol_id = 4
+
 exec insertEspecialidad @especialidad = 'neonatología';
 
 exec insertMedico @id_usuario = 6, @id_especialidad = 6
