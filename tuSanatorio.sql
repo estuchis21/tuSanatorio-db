62103b37e2d11b5520af388ef18f38625aee7c02
albano martinez <albano_107@hotmail.com>
2025-04-28 21:39:57 -0300

    SP VER TURNOS PACIENTES

---
+++ tuSanatorio.sql
@@ -0,0 +1,14 @@
+/*QUERY Ver sus turnos agendados.*/
+CREATE PROCEDURE MisTurnos
+    @id_usuario INT(50)
+AS
+BEGIN
+    SELECT ta.fecha_asignacion AS 'Mis Turnos'
+    FROM Pacientes p, Usuarios u, turnos_asignados ta
+    WHERE u.id_usuario = p.id_usuario
+      AND p.id_paciente = ta.id_paciente
+      AND u.id_usuario = @id_usuario;
+END;
+GO
+
+
