eb2286ac20265cb5fdd6461b20371b921dbfe36d
albano martinez <albano_107@hotmail.com>
2025-05-05 16:04:54 -0300

    SP corregido2 Asignacion de turnos de los pacientes

--- STORED PROCEDURES/Usuarios, medicos y pacientes/SELECTS/ASIGNAR_TURNOS.sql
+++ STORED PROCEDURES/Usuarios, medicos y pacientes/SELECTS/ASIGNAR_TURNOS.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE AsignarTurno
 	@id_turno INT
 AS
 BEGIN
-	SELECT td.fecha_turno as'Fecah del turno', um.nombres as 'Medico', e.especialidad as 'Especialidad'
+	SELECT td.fecha_turno as'Fecha del turno', um.nombres as 'Medico', e.especialidad as 'Especialidad'
 	FROM turnos_disponibles td, Medicos m , Usuarios um ,especialidades e
 	WHERE td.id_turno= @id_turno
 	AND	td.id_medico= m.id_medico
